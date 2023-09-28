059f932ca3d593a4efc1b7646323786243570b30
Facundo Lerena <faculerena@gmail.com>
2023-09-28 15:15:43 -0300

    Revert "upload"
    This reverts commit 66cbe8b58f14ca3c4122e57a33451f02749d0644.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { IdlContextProvider, IdlDataContext } from "@/components/idlContextProvider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <IdlContextProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </IdlContextProvider>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   )
 }
