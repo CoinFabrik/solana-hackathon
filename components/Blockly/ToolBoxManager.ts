58af32da293a0acf272c787578a80fbfa0feac81
Agustín Losiggio <arlosiggio@gmail.com>
2023-10-05 17:22:31 -0300

    first implementation of instruction blocks with normal accounts and signer accounts restriction

--- components/Blockly/ToolBoxManager.ts
+++ components/Blockly/ToolBoxManager.ts
@@ -5,7 +5,8 @@ import { ProgramWAddress } from "@/services/programLoader";
 import Blockly, { MenuOption, Toolbox } from "blockly/core";
 import javascript, { javascriptGenerator } from "blockly/javascript";
 import { MutableRefObject } from "react";
-import { IdlEnumVariant, IdlTypeDef } from "@coral-xyz/anchor/dist/cjs/idl";
+import { IdlEnumVariant, IdlType, IdlTypeDef, isIdlAccounts } from "@coral-xyz/anchor/dist/cjs/idl";
+
 class ToolboxManager {
     defaultToolbox: ToolboxInfo;
     accounts: Array<AccountName>;
@@ -13,6 +14,7 @@ class ToolboxManager {
     programs: Array<ProgramWAddress> = [];
     enumTypes: Array<any> = [];
     structTypes: Array<any> = [];
+    instructions: Array<any> = [];
     workspaceRef: MutableRefObject<Blockly.WorkspaceSvg>;
     constructor(
         workspaceRef: MutableRefObject<Blockly.WorkspaceSvg>,
@@ -37,7 +39,7 @@ class ToolboxManager {
                 this.appendDummyInput()
                     .appendField(new Blockly.FieldDropdown(accountsDropdownArr), "account");
                 this.setInputsInline(false);
-                this.setOutput(true, "String");
+                this.setOutput(true, "Account");
                 this.setColour(230);
                 this.setTooltip("");
                 this.setHelpUrl("");
@@ -59,7 +61,7 @@ class ToolboxManager {
                 this.appendDummyInput()
                     .appendField(new Blockly.FieldDropdown(signerDropdownArr), "signer");
                 this.setInputsInline(false);
-                this.setOutput(true, "String");
+                this.setOutput(true, "Signer");
                 this.setColour(0);
                 this.setTooltip("");
                 this.setHelpUrl("");
@@ -137,6 +139,64 @@ class ToolboxManager {
                   return [code, javascript.Order.NONE];
                 };
             });
+            this.instructions = [];
+            programs.map((program)=>{
+                console.log("program idl", program.idl)
+                program.idl.instructions.map((instruction)=>{
+                    let jsonDef = {
+                        "type": "block_type",
+                        "message0": instruction.name+"%1\nArguments:%2\n",
+                        "args0": [{
+                                "type": "input_dummy"
+                            },{
+                                "type": "input_dummy"
+                            }] as any[],
+                        "previousStatement": null,
+                        "nextStatement": null,
+                        "colour": 230,
+                        "tooltip": "",
+                        "helpUrl": ""
+                    }
+                    instruction.args.map((arg)=>{
+                        jsonDef.args0.push({
+                            "type": "input_value",
+                            "name": arg.name
+                            //aca hay que hacer un check con el tipo que deberia tener
+                        })
+                        jsonDef.message0 += arg.name + "%" + jsonDef.args0.length;
+
+                    });
+                    jsonDef.args0.push({
+                        "type": "input_dummy",
+                    })
+                    jsonDef.message0 += "Accounts:%"+jsonDef.args0.length+"\n"
+                    instruction.accounts.map((account)=>{
+                        if(!isIdlAccounts(account)){
+                            
+                            jsonDef.args0.push({
+                                "type": "input_value",
+                                "name": account.name,
+                                "check": account.isSigner?"Signer":"Account"
+                            })
+                            jsonDef.message0 += account.name + "%" + jsonDef.args0.length;
+                        }
+                    });
+                    console.log("jsonDef",jsonDef)
+                    Blockly.Blocks['program_'+program.idl.name+'_'+instruction.name] = {
+                        init: function() {
+                            this.jsonInit(jsonDef);
+                        }
+                    };
+                    javascriptGenerator.forBlock['program_'+program.idl.name+'_'+instruction.name] = function (
+                        block: Blockly.Block,
+                        generator: any
+                      ) {
+                        var code = 'program_'+program.idl.name+'_'+instruction.name;
+                        return code;
+                    };
+                    this.instructions.push('program_'+program.idl.name+'_'+instruction.name)
+                })
+            })
             this.workspaceRef.current.updateToolbox(this.generateToolbox());
         }
     }
@@ -183,6 +243,20 @@ class ToolboxManager {
                 "name": "Enums",
                 "colour": "#a587f7",
                 "contents": enums
+            })
+        }
+        if(this.instructions.length>0){
+            let instructions = this.instructions.map((instruction,i) => {
+                return {
+                    kind: "block",
+                    type: instruction,
+                };
+            })
+            customContent.push({
+                "kind": "category",
+                "name": "Instructions",
+                "colour": "#a587f7",
+                "contents": instructions
             })
         }
         let currentToolbox = { 
