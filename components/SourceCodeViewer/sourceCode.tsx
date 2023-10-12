8b936db77cc5539202ae4c3ab912d8b26fd2113a
Facundo Lerena <faculerena@gmail.com>
2023-10-12 13:22:37 -0300

    toasts for tests

--- components/SourceCodeViewer/sourceCode.tsx
+++ components/SourceCodeViewer/sourceCode.tsx
@@ -1,47 +1,61 @@
-'use client'
+"use client";
 // @ts-ignore
 import codePrettify from "code-prettify";
 import { Button } from "@/components/ui/button";
 import * as anchor from "@coral-xyz/anchor";
 
 import React, { useContext, useState } from "react";
-import './sourceCode.css';
+import "./sourceCode.css";
 import { AnchorProvider } from "@coral-xyz/anchor";
 import { Connection, Keypair } from "@solana/web3.js";
-import VirtualWallet from '@/services/virtualWallet';
+import VirtualWallet from "@/services/virtualWallet";
 import { ProgramsManagerContext } from "@/services/programLoader";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const SourceCode = ({source, ...props}: {source: any}) => {
+const SourceCode = ({ source, ...props }: { source: any }) => {
   const programsManager = useContext(ProgramsManagerContext);
-  
+
   const runCode = async () => {
-    if (typeof window !== 'undefined'){
-      console.log(source)
-      let tests: Array<[string,Function]> = [];
-      let results: Array<[boolean,string]> = [];
+    if (typeof window !== "undefined") {
+      console.log(source);
+      let tests: Array<[string, Function]> = [];
+      let results: Array<[boolean, string]> = [];
       (window as any).anchor = anchor;
       (window as any).it = (desc: string, fun: Function) => {
         tests.push([desc, fun]);
-      }
+      };
       (window as any).assert = (bool: boolean, expected: string) => {
         results.push([bool, expected]);
-      }
-      anchor.setProvider(new AnchorProvider(
-        new Connection(programsManager.networkInfo.networks[programsManager.networkInfo.selectedNetwork].url),
-        new VirtualWallet(Keypair.generate()),
-        AnchorProvider.defaultOptions()
-      ))
+      };
+      anchor.setProvider(
+        new AnchorProvider(
+          new Connection(
+            programsManager.networkInfo.networks[
+              programsManager.networkInfo.selectedNetwork
+            ].url
+          ),
+          new VirtualWallet(Keypair.generate()),
+          AnchorProvider.defaultOptions()
+        )
+      );
       let asyncfunc = eval(`async ()=>{${source}}`);
       await asyncfunc();
-      for(let test of tests){
+      for (let test of tests) {
         results = [];
-        console.log("Running test:", test[0],await test[1]());
-        let resMsg = results.filter((result)=>!result[0])
-            .reduce((msg, results)=>{
-              return `${msg}Assert failed: expected ${results[1]}\n`;
-            },"");
-        if(resMsg!=""){
-          alert(resMsg)
+        console.log("Running test:", test[0]);
+        let testResult = await test[1]();
+        if (results.every((result) => result[0])) {
+          // All assertions passed
+          toast.success(`Test "${test[0]}" PASSED`);
+        } else {
+          // Some assertions failed
+          let resMsg = results
+            .filter((result) => !result[0])
+            .reduce((msg, result) => {
+              return `${msg}Assert failed: expected ${result[1]}\n`;
+            }, "");
+          toast.error(`Test "${test[0]}" FAILED\n${resMsg}`);
         }
       }
     }
@@ -50,15 +64,18 @@ const SourceCode = ({source, ...props}: {source: any}) => {
     <>
       <div className="overflow-auto ">
         <pre className="prettyprint">
-          <code className="javascript" dangerouslySetInnerHTML={{__html: codePrettify.prettyPrintOne(source)}}>
-          </code>
+          <code
+            className="javascript"
+            dangerouslySetInnerHTML={{
+              __html: codePrettify.prettyPrintOne(source),
+            }}
+          ></code>
         </pre>
       </div>
-      <Button onClick={runCode}>
-        RUN
-      </Button>
+      <Button onClick={runCode}>RUN</Button>
+      <ToastContainer />
     </>
   );
 };
-  
+
 export default SourceCode;
